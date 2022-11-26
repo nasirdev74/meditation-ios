3eccc73deeae040337088716c3a84a8a65837c88
nasir <nasir.dev74@gmail.com>
2022-11-26 15:08:26 +0600

    Initial Commit

---
+++ meditation/meditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditationApp.swift
+//  meditation
+//
+//  Created by Softcent on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct meditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
